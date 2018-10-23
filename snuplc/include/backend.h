079cf8f8384d50607b4da5d95f203a7c9899bbc6
timedilation <timedilation@snu.ac.kr>
2018-10-23 14:12:30 +0900

    #4 8바이트 추가, 인스트럭션 추가, callee save 추가

--- snuplc/include/backend.h
+++ snuplc/include/backend.h
@@ -181,6 +181,107 @@ class CBackendx86 : public CBackend {
 };
 
 
+class CBackendx86_64 : public CBackend {
+  public:
+    /// @name constructors/destructors
+    /// @{
 
+    CBackendx86_64(ostream &out);
+    virtual ~CBackendx86_64(void);
+
+    /// @}
+
+  protected:
+    /// @name detailed output methods
+    /// @{
+
+    virtual void EmitHeader(void);
+    virtual void EmitCode(void);
+    virtual void EmitData(void);
+    virtual void EmitFooter(void);
+
+    /// @}
+
+    /// @name additional methods
+    /// @{
+
+    /// @brief set the current scope
+    void SetScope(CScope *scope);
+
+    /// @brief get the current scope
+    CScope* GetScope(void) const;
+
+    /// @brief emit a scope
+    virtual void EmitScope(CScope *scope);
+
+	/// regs stores used registers
+	/// if we tries to only save registers when necessary, we might have to use regs information.
+	/// 1: save, 0: don't save
+	/// bit order: (low) rbx, r12, r13, r13, r15 (high)
+	virtual void EmitCalleePush(char regs);
+	virtual void EmitCalleePop(char regs);
+	/// bit order: (low) r10, r11 (high)
+	///virtual void EmitCallerPush(char regs);
+	///virtual void EmitCallerPop(char regs);
+
+    /// @brief emit global data
+    virtual void EmitGlobalData(CScope *s);
+
+    /// @brief emit local data
+    ///
+    /// EmitLocalData() initializes local data (i.e., arrays)
+    virtual void EmitLocalData(CScope *s);
+
+    /// @brief emit code for code block @cb
+    virtual void EmitCodeBlock(CCodeBlock *cb);
+
+    /// @brief emit instruction @i
+    virtual void EmitInstruction(CTacInstr *i);
+
+    /// @brief emit an instruction
+
+    virtual void EmitInstruction(string mnemonic, string args="",
+                                 string comment="");
+
+    /// @brief emit a load instruction
+    void Load(CTacAddr *src, string dst, string comment="");
+
+    /// @brief emit a store instruction
+    void Store(CTac *dst, char src_base, string comment="");
+
+    /// @brief return an operand string for @a op
+    /// @param op the operand
+    string Operand(const CTac *op);
+
+    /// @brief return an immediate for @a value
+    string Imm(int value) const;
+
+    /// @brief return a x86-label for CTaclabel @a label
+    string Label(const CTacLabel *label) const;
+
+    /// @brief return a x86-label for string @a label
+    string Label(string label) const;
+
+    /// @brief return the condition suffix for a binary comparison operation
+    string Condition(EOperation cond) const;
+
+    /// @brief compute the size of operator @t
+    int OperandSize(CTac *t) const;
+
+    /// @brief compute the location of local variables, temporaries and
+    ///        arguments on the stack. Returns the total size occupied on
+    ///        the stack as well as the the number of arguments for this
+    ///        scope (if @a nargs is not NULL)
+    /// @param symtab symbol table
+    /// @param param_ofs offset to parameters from base pointer after epilogue
+    /// @param local_ofs offset to local vars from base pointer after epilogue
+    size_t ComputeStackOffsets(CSymtab *symtab, int param_ofs, int local_ofs);
+
+    /// @}
+
+    string _ind;                    ///< indentation
+    CScope *_curr_scope;            ///< current scope
+
+};
 
 #endif // __SnuPL_BACKEND_H__
