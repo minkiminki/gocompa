4dad39995696b1c3dd5c46f612f95210e0ae3138
minki <minki.cho@sf.snu.ac.kr>
2018-11-23 19:07:30 +0900

    register allocation on stack

--- snuplc/src/opt_ra.cpp
+++ snuplc/src/opt_ra.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <iomanip>
 #include <cassert>
+#include <map>
 
 #include "opt_ra.h"
 using namespace std;
@@ -53,6 +54,11 @@ void register_allocation_block(int arch, CSymtab *symtab, CCodeBlock *cb) {
   else
     local_ofs -= param_num * 8;
 
+
+  /* testing ... */
+  map<ERegister, int> reg_to_stack;
+  map<const CSymbol*, ERegister> & symb_to_reg = cbp->GetBlockTable()->GetLiveness()->GetAllocated();
+
   // iterate again to assign locals and params(>6) after get param num
   for (size_t i=0; i<slist.size(); i++) {
     CSymbol *s = slist[i];
@@ -61,28 +67,63 @@ void register_allocation_block(int arch, CSymtab *symtab, CCodeBlock *cb) {
     ESymbolType st = s->GetSymbolType();
 
     if (st == stLocal) {
-      int ssize = GetSize_prime(t);
-      int align = GetAlign_prime(t);
-      /*			if(t->IsPointer()) {
-				ssize = 8;
-				align = 8;
-				}
-      */
-      local_ofs -= ssize;
 
-      if ((align > 1) && (local_ofs % align != 0)) {
-        // align towards smaller addresses
-        align = (local_ofs - align +1) / align * align - local_ofs;
-      } else {
-        align = 0;
+      if(s->GetDataType()->IsArray()){
+	int ssize = GetSize_prime(t);
+	int align = GetAlign_prime(t);
+	local_ofs -= ssize;
+
+	if ((align > 1) && (local_ofs % align != 0)) {
+	  // align towards smaller addresses
+	  align = (local_ofs - align +1) / align * align - local_ofs;
+	} else {
+	  align = 0;
+	}
+
+	size += ssize - align;      // align is negative
+	local_ofs += align;
+
+	s->SetBaseRegister("");
+	// s->SetBaseRegister("%rbp");
+	s->SetOffset(local_ofs);
+      }
+      else{
+
+	if(symb_to_reg.find(s) == symb_to_reg.end()){
+	  cout << s << endl;
+	  _P1;
+	}
+	ERegister e = symb_to_reg[s];
+
+	if(reg_to_stack.find(e) == reg_to_stack.end()){
+	  int ssize = 8;
+	  int align = 8;
+	  local_ofs -= ssize;
+
+	  if ((align > 1) && (local_ofs % align != 0)) {
+	    // align towards smaller addresses
+	    align = (local_ofs - align +1) / align * align - local_ofs;
+	  } else {
+	    align = 0;
+	  }
+
+	  size += ssize - align;      // align is negative
+	  local_ofs += align;
+
+	  s->SetBaseRegister("");
+	  // s->SetBaseRegister("%rbp");
+	  s->SetOffset(local_ofs);
+	  // cout << ERegName[e] << " : " << local_ofs << endl;
+	  reg_to_stack[e] = local_ofs;
+	}
+	else{
+	  // local_ofs = reg_to_stack[e];
+	  s->SetBaseRegister("");
+	  // s->SetBaseRegister("%rbp");
+	  s->SetOffset(reg_to_stack[e]);
+	}
       }
 
-      size += ssize - align;      // align is negative
-      local_ofs += align;
-
-      s->SetBaseRegister("");
-      // s->SetBaseRegister("%rbp");
-      s->SetOffset(local_ofs);
     } else if (st == stParam) {
       if(param_num > 6){
 	CSymParam *p = dynamic_cast<CSymParam*>(s);
@@ -93,6 +134,11 @@ void register_allocation_block(int arch, CSymtab *symtab, CCodeBlock *cb) {
 	p->SetOffset(param_ofs - (p->GetIndex()-5)*8);
       }
     }
+
+    // cout << s << endl;
+    // cout << local_ofs << endl;
+
+
   }
   size = (size + sp_align-1) / sp_align * sp_align;
 
