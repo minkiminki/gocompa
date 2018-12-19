17400724a246a474cdeab4bd3436c6a17857ba7d
timedilation <timedilation@snu.ac.kr>
2018-12-19 20:54:49 +0900

    modify optimization

--- snuplc/src/opt.cpp
+++ snuplc/src/opt.cpp
@@ -10,8 +10,8 @@ void full_optimize(int arch, CScope *m) {
   // now pointer variables have proper types
   dofs_inlining_scope(m);
   // DIM and DOFS inlined
-  function_inlining_scope(m);
-  // unused_function_scope(m);
+  // function_inlining_scope(m);
+  unused_function_scope(m);
   pointer_typing_scope(m);
   register_promotion_scope(m);
   to_ir_prime_scope(m);
